d4b4a3765439d043e914b5d68a59fcea3c065c41
Liam O <lokanedeveloper@gmail.com>
2022-12-04 16:41:26 +1100

    title, array.map an button added to the DOM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "./components/Button";
 
 function App() {
+  const myArray = ["apple", "banana", "orange"];
+  const myList = myArray.map((item) => <p>{item}</p>);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World!</h1>
+      <div>{myList}</div>
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Button = () => {
+  return <button>This is a button to press</button>;
+};
